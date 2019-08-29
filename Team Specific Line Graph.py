83ba88be678bb08e0791ce4237afdd6ed9f37e42
Mason P <mason.phillips@barstowschool.org>
2019-08-29 13:34:21 -0500

    it takes the raw data and does the math so I can graph totals

--- Team Specific Line Graph.py
+++ Team Specific Line Graph.py
@@ -8,6 +8,35 @@ Created on Tue Aug 27 21:11:11 2019
 import pandas as pd
 from tkinter import filedialog
 import matplotlib.pyplot as plt
+
+def piecesMath(TeamDf):
+    TeamDf['telecargo'] = TeamDf['teleCargoCargo'] + TeamDf['TeleCargoHRocketCargo'] 
+    TeamDf['telecargo'] += TeamDf['TeleCargoMRocketCargo'] 
+    TeamDf['telecargo'] += TeamDf['TeleCargoLRocketCargo']
+  
+    TeamDf['sandcargo'] = TeamDf['SSCargoCargo'] + TeamDf['SSCargoSSHRocketCargo']
+    TeamDf['sandcargo'] += TeamDf['SSCargoSSMRocketCargo']
+    TeamDf['sandcargo'] += TeamDf['SSCargoSSLRocketCargo']
+    
+    TeamDf['telehatch'] = TeamDf['teleCargoHatch'] + TeamDf['TeleHatchHRocketHatch']
+    TeamDf['telehatch'] += TeamDf['TeleHatchMRocketHatch']
+    TeamDf['telehatch'] += TeamDf['TeleHatchLRocketHatch']
+    
+    TeamDf['sandhatch'] = TeamDf['SSCargoHatch'] + TeamDf['SSCargoSSHRocketHatch']
+    TeamDf['sandhatch'] += TeamDf['SSCargoSSMRocketHatch']
+    TeamDf['sandhatch'] += TeamDf['SSCargoSSLRocketHatch']
+    
+    TeamDf['totalscored'] = TeamDf['telecargo'] + TeamDf['sandcargo']
+    TeamDf['totalscored'] += TeamDf['telehatch']
+    TeamDf['totalscored'] += TeamDf['sandhatch']
+    
+    TeamDf['teletotal'] = TeamDf['telecargo'] + TeamDf['telehatch']
+    
+    TeamDf['sandtotal'] = TeamDf['sandcargo'] + TeamDf['sandhatch']
+    
+    TeamDf['totalcargo'] = TeamDf['telecargo'] + TeamDf['sandcargo']
+    
+    TeamDf['totalhatch'] = TeamDf['telehatch'] + TeamDf['sandhatch']
 #import numpy
 
 team = int(input('Enter Which Team you want to generate a graph for:'))
@@ -16,14 +45,15 @@ print('Enter 1 to generate a sandstorm graph')
 print('Enter 2 to generate a tele graph')
 selection = input('Enter your selection here: ')
 
-df = pd.read_csv(filedialog.askopenfilename(title = 'select MatchList file'), sep = ',')
-df.set_index("team", inplace = True)
+df = pd.read_csv(filedialog.askopenfilename(title = 'select MatchList file'), sep = '|')
+df.set_index("teamNo", inplace = True)
 
+piecesMath(df)
 
-print(df.loc[[team], ["match"]])
-print(df.loc[[team], ["totalScored"]])
-print(df.loc[[team], ["totalCargo"]])
-print(df.loc[[team], ["totalHatch"]])
+print(df.loc[[team], ["matchNo"]])
+#print(df.loc[[team], ["totalscored"]])
+#print(df.loc[[team], ["totalCargo"]])
+#print(df.loc[[team], ["totalHatch"]])
 
 if selection == "0":
     plt.figure()
@@ -32,17 +62,17 @@ if selection == "0":
 
     plt.subplot(311)
     plt.title('Total Pieces')
-    plt.plot(df.loc[[team], ["match"]], df.loc[[team], ["totalScored"]])
+    plt.plot(df.loc[[team], ["matchNo"]], df.loc[[team], ["totalscored"]])
     plt.ylabel('Total Objects')
     
     plt.subplot(312)
     plt.title('Total Cargo')
-    plt.plot(df.loc[[team], ["match"]], df.loc[[team], ["totalCargo"]], color="green")
+    plt.plot(df.loc[[team], ["matchNo"]], df.loc[[team], ["totalcargo"]], color="green")
     plt.ylabel('Cargo')
 
     plt.subplot(313)
     plt.title('Total Hatch')
-    plt.plot(df.loc[[team], ["match"]], df.loc[[team], ["totalHatch"]], color="red")
+    plt.plot(df.loc[[team], ["matchNo"]], df.loc[[team], ["totalhatch"]], color="red")
     plt.ylabel('HP')
     
 if selection == "1":
@@ -51,17 +81,17 @@ if selection == "1":
 
     plt.subplot(311)
     plt.title('Total Sand')
-    plt.plot(df.loc[[team], ["match"]], df.loc[[team], ["totalSand"]])
+    plt.plot(df.loc[[team], ["matchNo"]], df.loc[[team], ["sandtotal"]])
     plt.ylabel('Total Objects')
     
     plt.subplot(312)
     plt.title('Total Sand Cargo')
-    plt.plot(df.loc[[team], ["match"]], df.loc[[team], ["sandcargo"]], color="green")
+    plt.plot(df.loc[[team], ["matchNo"]], df.loc[[team], ["sandcargo"]], color="green")
     plt.ylabel('Cargo')
 
     plt.subplot(313)
     plt.title('Total Sand Hatch')
-    plt.plot(df.loc[[team], ["match"]], df.loc[[team], ["sandhatch"]], color="red")
+    plt.plot(df.loc[[team], ["matchNo"]], df.loc[[team], ["sandhatch"]], color="red")
     plt.ylabel('HP')
 
 if selection == "2":
@@ -70,16 +100,16 @@ if selection == "2":
 
     plt.subplot(311)
     plt.title('Total Tele Pieces')
-    plt.plot(df.loc[[team], ["match"]], df.loc[[team], ["totalTele"]])
+    plt.plot(df.loc[[team], ["matchNo"]], df.loc[[team], ["teletotal"]])
     plt.ylabel('Total Objects')
     
     plt.subplot(312)
     plt.title('Total Tele Cargo')
-    plt.plot(df.loc[[team], ["match"]], df.loc[[team], ["telecargo"]], color="green")
+    plt.plot(df.loc[[team], ["matchNo"]], df.loc[[team], ["telecargo"]], color="green")
     plt.ylabel('Cargo')
 
     plt.subplot(313)
     plt.title('Total Tele Hatch')
-    plt.plot(df.loc[[team], ["match"]], df.loc[[team], ["telehatch"]], color="red")
+    plt.plot(df.loc[[team], ["matchNo"]], df.loc[[team], ["telehatch"]], color="red")
     plt.ylabel('HP')
 
